fc3d0ab325e62f57539fd85a8b221358873ed8e5
erosMariano <erosmariano1@gmail.com>
2021-12-08 14:52:14 -0300

    att

---
+++ criptoativos/script.js
@@ -0,0 +1,3 @@
+document.querySelector("#element-112 .x_39a3bfb4").innerHTML = "Invista em um dos 3 fundos com exposição a criptomoedas"
+
+document.querySelector("#element-112 .x_652bb576").style.width = "633px"
