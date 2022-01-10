06d958681f035434cc87761a75852329394f6327
erosMariano <erosmariano1@gmail.com>
2022-01-10 14:01:18 -0300

    add

---
+++ XP/teste6/scriopt.js
@@ -0,0 +1,13 @@
+const sectionTransition = document.querySelector("#page-block-eaukgakfxfa")
+
+const newSection = sectionTransition.cloneNode(true)
+document.querySelector("#page-block-l2bhiinpaoi").insertAdjacentElement("afterend", newSection)
+
+sectionTransition.style.display="none"
+
+
+document.querySelector(".x_938e8cfc .x_c5f0bf77").display = "none";
+
+
+document.querySelector("#element-3185 .x_7b2817bf").innerHTML = "Estoque limitado sujeito a disponibilidade e taxas indicativas."
+document.querySelector("#element-3184 strong").innerHTML = "<span>Além do CDB de 14%,</span> confira outras rendas fixas de 1%"
