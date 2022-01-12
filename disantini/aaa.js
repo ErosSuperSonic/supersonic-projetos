aba739f5f8a3924cea32f0d5f530bb711df91dbd
erosMariano <erosmariano1@gmail.com>
2022-01-12 17:31:36 -0300

    att

---
+++ disantini/aaa.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector("main button");
+btn.addEventListener("click", () => {
+	btn.innerHTML = `<div class="loading"></div>`;
+
+});
--- disantini/script.js
+++ disantini/script.js
@@ -1,16 +1,9 @@
-const favorito = document.querySelector(
-	"button.btn.icon-button.add-to-wish-list"
-); //Instanciando botao favorido
+const favorito = document.querySelector("button.btn.icon-button.add-to-wish-list"); //Instanciando botao favorido
 
 const newBtnFavorito = favorito;
-//favorito.style.display = "none"
-
-
 
 //botoes de quantidade e tamanho
-const tamanho = document.querySelector(
-	"div.attributes>div:nth-of-type(3)>div:nth-of-type(1)"
-);
+const tamanho = document.querySelector("div.attributes>div:nth-of-type(3)>div:nth-of-type(1)");
 const quantidade = document.querySelector("div.quantity");
 
 tamanho.classList.replace("col-6", "col-5");
@@ -23,8 +16,8 @@ tamanho.insertAdjacentHTML(
     </button>`
 );
 
-//Adionando e Ativando popup
 
+//Adionando e Ativando popup
 const btnGuiaTamanhos = document.querySelector(".guia-tamanho-ss");
 const maincontent = document.querySelector("#maincontent");
 
@@ -136,24 +129,20 @@ maincontent.insertAdjacentHTML(
 );
 
 btnGuiaTamanhos.addEventListener("click", () => {
-	document
-		.querySelector(".guia-tamanhos-ss-popup")
-		.classList.toggle("active-ss");
+    document.querySelector(".guia-tamanhos-ss-popup").classList.toggle("active-ss");
 });
 
 const btnClosePopup = document.querySelector(".close-popup-ss");
 btnClosePopup.addEventListener("click", () => {
-	document
-		.querySelector(".guia-tamanhos-ss-popup")
-		.classList.toggle("active-ss");
+	document.querySelector(".guia-tamanhos-ss-popup").classList.toggle("active-ss");
 });
 
 
 
-
-//========Adicionando nova seção
-
-document.querySelector("div.attributes>div:nth-of-type(3)").insertAdjacentHTML("afterend", `
+//======== Adicionando nova seção ========//
+document.querySelector("div.attributes>div:nth-of-type(3)").insertAdjacentHTML(
+	"afterend",
+	`
 <div class="infos-ss">
 <div id="container-flex">
     <div style="align-items: center; display: flex;">
@@ -175,47 +164,112 @@ document.querySelector("div.attributes>div:nth-of-type(3)").insertAdjacentHTML("
     <p><strong><span>Retire grátis</span></strong> <span>em qualquer loja física.</span></p>
 </div>
 </div>
-`)
-
+`
+);
 
 
 //Botão adicionar carrinho
-
-//Entender pq o botào não funciona
-const infoSs = document.querySelector(".infos-ss")
-const btnAddCarrinho = document.querySelector("button.btn.btn-sucess.btn-block.add-to-cart")
-const newBtnAddCarrinho = btnAddCarrinho
-
-infoSs.insertAdjacentElement("afterend", newBtnAddCarrinho)
-btnAddCarrinho.insertAdjacentElement("afterend",  newBtnFavorito)
+const infoSs = document.querySelector(".infos-ss");
+const btnAddCarrinho = document.querySelector("button.btn.btn-sucess.btn-block.add-to-cart");
+const newBtnAddCarrinho = btnAddCarrinho;
+infoSs.insertAdjacentElement("afterend", newBtnAddCarrinho);
+btnAddCarrinho.insertAdjacentElement("afterend", newBtnFavorito);
 
 
 newBtnFavorito.innerHTML = `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M14.4219 4.43126C14.2124 3.94632 13.9104 3.50687 13.5328 3.13751C13.1549 2.76705 12.7093 2.47265 12.2203 2.27032C11.7132 2.05969 11.1694 1.95187 10.6203 1.95314C9.85 1.95314 9.09844 2.16407 8.44531 2.56251C8.28906 2.65782 8.14062 2.76251 8 2.87657C7.85938 2.76251 7.71094 2.65782 7.55469 2.56251C6.90156 2.16407 6.15 1.95314 5.37969 1.95314C4.825 1.95314 4.2875 2.05939 3.77969 2.27032C3.28906 2.47345 2.84687 2.76564 2.46719 3.13751C2.08906 3.50645 1.787 3.94601 1.57812 4.43126C1.36094 4.93595 1.25 5.47189 1.25 6.02345C1.25 6.54376 1.35625 7.08595 1.56719 7.63751C1.74375 8.09845 1.99687 8.57657 2.32031 9.05938C2.83281 9.82345 3.5375 10.6203 4.4125 11.4281C5.8625 12.7672 7.29844 13.6922 7.35938 13.7297L7.72969 13.9672C7.89375 14.0719 8.10469 14.0719 8.26875 13.9672L8.63906 13.7297C8.7 13.6906 10.1344 12.7672 11.5859 11.4281C12.4609 10.6203 13.1656 9.82345 13.6781 9.05938C14.0016 8.57657 14.2562 8.09845 14.4312 7.63751C14.6422 7.08595 14.7484 6.54376 14.7484 6.02345C14.75 5.47189 14.6391 4.93595 14.4219 4.43126V4.43126Z" fill="#4A4A4A"/>
-</svg> <strong>Adicionar aos favoritos</strong>`
+</svg> <strong>Adicionar aos favoritos</strong>`;
 //adicionando botão de favorito
 
 
 
+//Criando elemento para definir a cor para o usuário
+document.querySelector(".attribute.color").insertAdjacentHTML("afterbegin","<p class='paragrafo-color-add'><strong>Cor:</strong><span class='cor-value'></span></p>");
+document.querySelector(".product-detail-attributes div").classList.replace("col-12", "col-10");
 
-
-
+document.querySelector(".cor-value").innerHTML = document.querySelectorAll("button.color-attribute")[0].title;
 
 //Pegando cor dos botões
-document.querySelectorAll("button.color-attribute").forEach((button) =>{
-    button.addEventListener("click", () =>{
-        console.log(`Cliquei no botão da cor ${button.title}`)
-    })
-})
+document.querySelectorAll("button.color-attribute").forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(`Cliquei no botão da cor ${button.title}`);
+		document.querySelector(".cor-value").innerHTML = button.title; //Definindo nome da cor no Desktop
+        document.querySelectorAll(".cor-value")[1].innerHTML =  button.title; //Definindo nome da cor no mobile
+	});
+});
+
+
+
+
+
+if (window.innerWidth <= "992") {
+    tamanho.classList.replace("col-5", "col-6");
+    quantidade.classList.replace("col-3", "col-6");
+
+    btnGuiaTamanhos.style.display="none"
+    btnGuiaTamanhos.classList.remove("col-4")
+
+    const btnGuiaTamanhosMobile = btnGuiaTamanhos;
+    btnGuiaTamanhosMobile.style.display = "flex"
+    infoSs.insertAdjacentElement("beforebegin", btnGuiaTamanhosMobile)
+}
+
+    
+if(window.innerWidth <= 600){
+    const linhaCarrousel = document.querySelector("div.col-10")
+    linhaCarrousel.classList.replace("col-10", "col-8")
+    linhaCarrousel.classList.add("caroussel-elements")
+
+    document.querySelectorAll(".product-detail-attributes")[1].insertAdjacentHTML("afterbegin","<p class='paragrafo-color-add'><strong>Cor:</strong><span class='cor-value'></span></p>");
+    document.querySelectorAll(".cor-value")[1].innerHTML = document.querySelectorAll("button.color-attribute")[0].title;
+}
+
+
+
+
+
+
+
+
 
 
 //Puxando dados com api
-async function fetchProducts(){
-    const response = await fetch("https://www.disantinni.com.br/on/demandware.store/Sites-di-santinni-Site/default/Product-Variation?dwvar_74675_color=74675018&dwvar_74675_size=35&pid=74675&quantity=1")
-    return response
+
+const myCollection = document.querySelectorAll(".attribute.color")[0].children; //Pegando o container com os quadrados das imagens
+const arrImages = [];
+
+function chamarDados() {
+
+        console.log("Entrei camada 1")
+        Array.from(myCollection).forEach((el) => {
+            //Begando os botões com as urls para as imagens
+            if (el.tagName === "BUTTON") {
+                fetch(el.attributes["data-url"].value)
+                    .then(function (response) {
+                        console.log("Entrei camada 2")
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        console.log("Entrei camada 3")
+                        el.querySelector("span.color-value").style.backgroundImage = `url('${data.product.images.large[0].absUrl}')`;
+                        console.log(el.querySelector("span.color-value"))
+                    })
+                    .catch((error) => {
+                        console.log("Deu erro", error);
+                    });
+            }
+        })
+    
 }
 
-fetchProducts().then(product => {
-    console.log(product); // fetched movies
-});
+
+chamarDados();
+
+
+
+
+
+
+
+
 
