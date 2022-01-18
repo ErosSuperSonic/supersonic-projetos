7bad4bb1bfc32473d763a529d7443f9cc981f946
erosMariano <erosmariano1@gmail.com>
2022-01-18 13:15:58 -0300

    att

---
+++ clear/teste6/script.js
@@ -0,0 +1,11 @@
+document.querySelector(".x_95fc3c9c").innerHTML = `<p class="new-title" style="color:#00FA7E;">Curso online gratuito de <span style="font-weight: 700;">Renda Variável</span></p>`
+document.querySelector("#element-441").style.width = "29.375rem"
+document.querySelector("#element-441").style.height = "0px"
+
+
+//removendo copy
+document.querySelector("#element-442.paragraph").style.display = "none"
+
+//adicionando copy nova
+
+document.querySelector(".x_95fc3c9c").insertAdjacentHTML("afterend", `<p class="new-copy">Aprenda a investir do zero com grandes especialistas. Cadastre abaixo e tenha acesso ao curso:  <strong>Start na Renda  Variável</strong></p>`)
