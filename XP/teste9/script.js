54b613c1aaecad2b14300446cdfc93bae475139b
erosMariano <erosmariano1@gmail.com>
2022-01-28 11:01:26 -0300

    att

---
+++ XP/teste9/script.js
@@ -0,0 +1,45 @@
+document.querySelector("div#element-32").innerHTML = `<h1 class="h1-ss">Invista de forma completa na <span style="color: #FBC105">maior corretora do Brasil</span></h1>`
+
+document.querySelector("div#element-637").insertAdjacentHTML("beforeend", `<p class="copy-xp">Veja o que só quem é XP vê</p>`)
+
+document.querySelector("div#element-637").insertAdjacentHTML("beforeend", `
+<div class="container-lista-ss">
+        <ul>
+            <li>
+                <svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M10 20.3333C14.6024 20.3333 18.3333 16.6024 18.3333 12C18.3333 7.39763 14.6024 3.66667 10 3.66667C5.39763 3.66667 1.66667 7.39763 1.66667 12C1.66667 16.6024 5.39763 20.3333 10 20.3333ZM10 22C4.47715 22 0 17.5228 0 12C0 6.47715 4.47715 2 10 2C15.5228 2 20 6.47715 20 12C20 17.5228 15.5228 22 10 22ZM8.8058 14.2011L5.58908 10.9863L4.41092 12.1652L8.80746 16.559L15.8733 9.47749L14.6935 8.30029L8.8058 14.2011Z"
+                        fill="white" />
+                </svg>
+                Mais de 38 anos de história
+            </li>
+
+            <li>
+                <svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M10 20.3333C14.6024 20.3333 18.3333 16.6024 18.3333 12C18.3333 7.39763 14.6024 3.66667 10 3.66667C5.39763 3.66667 1.66667 7.39763 1.66667 12C1.66667 16.6024 5.39763 20.3333 10 20.3333ZM10 22C4.47715 22 0 17.5228 0 12C0 6.47715 4.47715 2 10 2C15.5228 2 20 6.47715 20 12C20 17.5228 15.5228 22 10 22ZM8.8058 14.2011L5.58908 10.9863L4.41092 12.1652L8.80746 16.559L15.8733 9.47749L14.6935 8.30029L8.8058 14.2011Z"
+                        fill="white" />
+                </svg>
+                Mais de 1 milhão de cilentes
+            </li>
+
+
+            <li>
+                <svg width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M10 20.3333C14.6024 20.3333 18.3333 16.6024 18.3333 12C18.3333 7.39763 14.6024 3.66667 10 3.66667C5.39763 3.66667 1.66667 7.39763 1.66667 12C1.66667 16.6024 5.39763 20.3333 10 20.3333ZM10 22C4.47715 22 0 17.5228 0 12C0 6.47715 4.47715 2 10 2C15.5228 2 20 6.47715 20 12C20 17.5228 15.5228 22 10 22ZM8.8058 14.2011L5.58908 10.9863L4.41092 12.1652L8.80746 16.559L15.8733 9.47749L14.6935 8.30029L8.8058 14.2011Z"
+                        fill="white" />
+                </svg>
+                Mais de R$715 bilhões sob custódia
+            </li>
+        </ul>
+    </div>
+`)
+
+
+document.querySelector("div#element-637").insertAdjacentHTML("beforeend", `
+<div class="container-button">
+        <a href="https://cadastro.xpi.com.br/desktop/step/1">Abra sua conta</a>
+        <p>Leva só 5 minutos</p>
+    </div>
+`)
