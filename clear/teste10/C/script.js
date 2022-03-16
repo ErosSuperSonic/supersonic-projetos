b03673fdc44fd3039ad3b7b5b661a51c767d5f72
erosMariano <erosmariano1@gmail.com>
2022-03-16 13:42:27 -0300

    att

---
+++ clear/teste10/C/script.js
@@ -0,0 +1,28 @@
+const myConteudo = `<div class="container-ss">
+        <div class="logo">
+            <img src="https://raw.githubusercontent.com/ErosSuperSonic/supersonic-projetos/main/clear/teste10/logotipo.png"
+                alt="" srcset="">
+        </div>
+
+        <div class="container-text">
+            <div class="coluna1TextSS">
+                <p>De 28/03 a 06/05</p>
+                <p>Concorra a mais de</p>
+                <p>R$400 mil</p>
+                <p>em prêmios*</p>
+            </div>
+
+            <div class="coluna2TextSS">
+                <p>Abra sua conta e prove que <span>você é o melhor trader do Brasil.</span></p>
+            </div>
+        </div>
+        <div class="copy">
+            <p>Período de inscrição: de 14.03.2022 às 12h ao dia 05.05.2022. Consulte os regulamentos completos e
+                Certificados de
+                Autorização no site https://www.ligaclear.com.br/regulamento.aspx
+                *Soma dos prêmios distribuídos ao longo de todo o período do concurso.
+                *Premiação entregue em carteira de investimento com cotas do Tesouro Direto Selic*.</p>
+        </div>
+    </div>`
+
+document.querySelector(".hero__container").insertAdjacentHTML("afterbegin", myConteudo)
