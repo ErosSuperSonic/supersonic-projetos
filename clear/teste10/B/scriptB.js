81db0460f051d5eacc6438af8e27ee327e4fca6f
erosMariano <erosmariano1@gmail.com>
2022-03-16 14:39:04 -0300

    att

--- clear/teste10/B/scriptB.js
+++ clear/teste10/B/scriptB.js
@@ -1,21 +1,20 @@
-const myConteudo = ` <div class="container-ss">
+const myConteudo = `    <div class="container-ss">
         <div class="logo">
-            <img src="https://raw.githubusercontent.com/ErosSuperSonic/supersonic-projetos/main/clear/teste10/logotipo.png"
+            <img src="https://raw.githubusercontent.com/ErosSuperSonic/supersonic-projetos/main/clear/teste10/C/logotipo.png"
                 alt="" srcset="">
         </div>
-    
+
         <div class="container-text">
             <div class="coluna1TextSS">
-                <p class="data">De 28/03 a 06/05</p>
-                <p class="concorra">Concorra a mais de</p>
-                <p class="valor">R$400 mil</p>
-                <p class="emPremios">em prêmios*</p>
+                <img src="https://raw.githubusercontent.com/ErosSuperSonic/supersonic-projetos/main/clear/teste10/B/Headline.png"
+                    alt="De 28/03 a 06/05, concorra a mais de 400 mil reais em premios" srcset="">
             </div>
-    
+
             <div class="coluna2TextSS">
                 <p>Abra sua conta e prove que <span>você é o melhor trader do Brasil.</span></p>
             </div>
         </div>
+        <a href="https://cadastro.clear.com.br/" class="abraConta">Abra sua conta</a>
         <div class="copy">
             <p>Período de inscrição: de 14.03.2022 às 12h ao dia 05.05.2022. Consulte os regulamentos completos e
                 Certificados de
