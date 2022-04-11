8c3c9efdd79e99cc6b7850e85eb454a160472a86
erosMariano <erosmariano1@gmail.com>
2022-04-11 14:48:21 -0300

    add

--- clear/teste 11/script.js
+++ clear/teste 11/script.js
@@ -1,2 +1,19 @@
-const myText =  
-document.querySelector(".clear-hero").insertAdjacentHTML("beforeend", "<p>Eros</p>")
+const myText = `
+<div class="maxContainer">
+        <div class="container-ss">
+            <img src="https://github.com/ErosSuperSonic/supersonic-projetos/blob/main/clear/teste%2011/xpinc.png?raw=true"
+                alt="">
+            <img src="https://github.com/ErosSuperSonic/supersonic-projetos/blob/main/clear/teste%2011/reclame%20aqui.png?raw=true"
+                alt="">
+            <img src="https://github.com/ErosSuperSonic/supersonic-projetos/blob/main/clear/teste%2011/ibest.png?raw=true"
+                alt="">
+        </div>
+    </div>
+`
+document.querySelector(".clear-hero").insertAdjacentHTML("beforeend", myText)
+
+
+
+if (window.location.pathname === "/fundos-de-investimento-imobiliarios/" && window.innerWidth >= 1084){
+    document.querySelector(".maxContainer").style.bottom = "6%"
+}
