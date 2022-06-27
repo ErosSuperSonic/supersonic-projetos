d159e57e9a18565ea552629caf974f659f34e6a6
erosMariano <erosmariano1@gmail.com>
2022-06-27 12:52:17 -0300

    att

---
+++ XP/teste1/script.js
@@ -0,0 +1,55 @@
+
+let content = `    <div class="hero-ss">
+        <div class="container">
+            <h1>Potencialize os resultados da sua empresa através de soluções financeiras completas</h1>
+
+            <ul>
+                <li>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M16.0003 29.3334C23.3641 29.3334 29.3337 23.3639 29.3337 16.0001C29.3337 8.63628 23.3641 2.66675 16.0003 2.66675C8.63653 2.66675 2.66699 8.63628 2.66699 16.0001C2.66699 23.3639 8.63653 29.3334 16.0003 29.3334Z"
+                            stroke="white" stroke-width="1.5" />
+                        <path d="M21.3337 12L13.3337 20L10.667 17.3333" stroke="white" stroke-width="1.5" />
+                    </svg>
+                    Plataforma de investimentos
+                </li>
+
+                <li>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M16.0003 29.3334C23.3641 29.3334 29.3337 23.3639 29.3337 16.0001C29.3337 8.63628 23.3641 2.66675 16.0003 2.66675C8.63653 2.66675 2.66699 8.63628 2.66699 16.0001C2.66699 23.3639 8.63653 29.3334 16.0003 29.3334Z"
+                            stroke="white" stroke-width="1.5" />
+                        <path d="M21.3337 12L13.3337 20L10.667 17.3333" stroke="white" stroke-width="1.5" />
+                    </svg>
+                    Taxa zero de abertura e manutenção
+                </li>
+
+
+                <li>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M16.0003 29.3334C23.3641 29.3334 29.3337 23.3639 29.3337 16.0001C29.3337 8.63628 23.3641 2.66675 16.0003 2.66675C8.63653 2.66675 2.66699 8.63628 2.66699 16.0001C2.66699 23.3639 8.63653 29.3334 16.0003 29.3334Z"
+                            stroke="white" stroke-width="1.5" />
+                        <path d="M21.3337 12L13.3337 20L10.667 17.3333" stroke="white" stroke-width="1.5" />
+                    </svg>
+                    Assessoria de investimentos
+                </li>
+
+
+
+                <li>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M16.0003 29.3334C23.3641 29.3334 29.3337 23.3639 29.3337 16.0001C29.3337 8.63628 23.3641 2.66675 16.0003 2.66675C8.63653 2.66675 2.66699 8.63628 2.66699 16.0001C2.66699 23.3639 8.63653 29.3334 16.0003 29.3334Z"
+                            stroke="white" stroke-width="1.5" />
+                        <path d="M21.3337 12L13.3337 20L10.667 17.3333" stroke="white" stroke-width="1.5" />
+                    </svg>
+                    Produtos de câmbio, derivativos e crédito
+                </li>
+            </ul>
+
+            <a href="https://cadastro.xpempresas.com.br/step1">ABRA SUA CONTA</a>
+        </div>
+    </div>`
+document.querySelector("div.clients").insertAdjacentHTML("beforebegin", content)
+
