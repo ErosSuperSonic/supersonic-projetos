b6b2498bd68fa584912997ef33f33a3d13fbc9ef
erosMariano <eros.mariano@supersonic.ag>
2022-09-23 16:42:19 -0300

    update projeto sprint45XP

--- XP/sprint45XP/script.js
+++ XP/sprint45XP/script.js
@@ -1,8 +1,8 @@
-//Resets
-if(window.innerWidth > 768){
-  document.querySelector("#element-568").style.width = "100%"
-document.querySelector("#page-block-gy918ronptb > div.section-block > div").style.maxWidth = "100%" 
-}
+// //Resets
+// if(window.innerWidth > 768){
+//   document.querySelector("#element-568").style.width = "100%"
+// document.querySelector("#page-block-gy918ronptb > div.section-block > div").style.maxWidth = "100%"
+// }
 
 String.prototype.reverse = function () {
   return this.split("").reverse().join("");
@@ -25,6 +25,8 @@ function mascaraMoeda(campo, evento) {
     }
   }
   campo.value = resultado.reverse();
+
+  return (campo.value = resultado.reverse());
 }
 
 const quantidadeInvestidaInput = document.querySelector(
@@ -32,9 +34,15 @@ const quantidadeInvestidaInput = document.querySelector(
 );
 const diasInvestidosInput = document.querySelector(".diasInvestidos input");
 const rendimentoCDI = document.querySelector(".rendimentoCDI h4");
-const lucroCDBCampo = document.querySelector(".lucroCDB");
-const lucroSemIOF = document.querySelector(".lucroSemIOF");
+// const lucroCDBCampo = document.querySelector(".lucroCDB");
+// const lucroSemIOF = document.querySelector(".lucroSemIOF");
 const CDI = 13.65 / 100;
+
+//Variáveis tabela
+
+const tableInfo = document.querySelectorAll(".tableInfo td");
+const infoTableList = document.querySelectorAll(".listWithInfosInGraph .value");
+
 //========== Quantidade investida ============
 const [quantidadeInvestidaMenos, quantidadeInvestidaMais] =
   document.querySelectorAll(
@@ -52,6 +60,9 @@ quantidadeInvestidaInput.addEventListener("blur", (e) => {
   }
   LucroComCdb();
   LucroComCompromissada();
+
+  const valueForTable = mascaraMoeda(quantidadeInvestidaInput);
+  setVariablesTable(5, valueForTable);
 });
 
 quantidadeInvestidaMais.addEventListener("click", (e) => {
@@ -64,6 +75,9 @@ quantidadeInvestidaMais.addEventListener("click", (e) => {
   mascaraMoeda(quantidadeInvestidaInput);
   LucroComCdb();
   LucroComCompromissada();
+
+  const valueForTable = mascaraMoeda(quantidadeInvestidaInput);
+  setVariablesTable(5, valueForTable);
 });
 
 quantidadeInvestidaMenos.addEventListener("click", (e) => {
@@ -81,6 +95,9 @@ quantidadeInvestidaMenos.addEventListener("click", (e) => {
   }
   LucroComCdb();
   LucroComCompromissada();
+
+  const valueForTable = mascaraMoeda(quantidadeInvestidaInput);
+  setVariablesTable(5, valueForTable);
 });
 
 //========== Dias investidos na compromissada ============
@@ -133,31 +150,28 @@ function LucroComCdb() {
     (quantidadeInvestidaValue - valorBruto).toFixed(2) * -1;
   let IOF = 0;
 
+  function infoToday() {
+    let day = new Date().getDate();
+    let month = new Date().getMonth() + 1;
+    let year = new Date().getFullYear();
 
-  function infoToday(){
-    let day = new Date().getDate()
-    let month = new Date().getMonth() + 1
-    let year = new Date().getFullYear()
-
-    return `${month}/${day}/${year}`
+    return `${month}/${day}/${year}`;
   }
-  
+
   var day1 = new Date(infoToday());
   let day2 = new Date("09/05/2022");
 
-  if(dias === "01"){
-    day2 = new Date("08/30/2022");
-  }else if(dias === "03"){
-    day2 = new Date("09/01/2022");
-  }else if(dias === "05"){
-    day2 = new Date("09/05/2022");
-  }else if(dias === "07"){
-    day2 = new Date("09/08/2022");
-  }else{
-    day2 = new Date("09/13/2022");
+  if (dias === "01") {
+    day2 = new Date("09/26/2022");
+  } else if (dias === "03") {
+    day2 = new Date("09/28/2022");
+  } else if (dias === "05") {
+    day2 = new Date("09/30/2022");
+  } else if (dias === "07") {
+    day2 = new Date("10/04/2022");
+  } else {
+    day2 = new Date("10/07/2022");
   }
-
-
 
   var difference = Math.abs(day2 - day1);
   diasEntreAsDatas = difference / (1000 * 3600 * 24);
@@ -179,9 +193,28 @@ function LucroComCdb() {
 
   const lucroCDB = (valorLiquido - quantidadeInvestidaValue).toFixed(2);
 
-  lucroCDBCampo.innerHTML = `R$ ${lucroCDB.replace(".", ",")}`;
+  // lucroCDBCampo.innerHTML = `R$ ${lucroCDB.replace(".", ",")}`;
 
-  rendimentoPorcentagemCDI(dias)
+  const applicationData = `${day1.getDate()}/${
+    day1.getMonth() <= 10
+      ? String(day1.getMonth()).padStart(2, "0")
+      : day1.getMonth()
+  }/${day1.getFullYear()}`;
+
+  const dueDate = `${
+    day2.getDate() <= 10
+      ? String(day2.getDate()).padStart(2, "0")
+      : day2.getDate()
+  }/${
+    day2.getMonth() <= 10
+      ? String(day2.getMonth()).padStart(2, "0")
+      : day2.getMonth()
+  }/${day2.getFullYear()}`;
+
+  setVariablesTable(6, applicationData);
+  setVariablesTable(7, dias);
+  setVariablesTable(8, dueDate);
+  rendimentoPorcentagemCDI(dias);
 }
 
 //===========  Lucro com Compromissada ===========
@@ -221,11 +254,10 @@ function LucroComCompromissada() {
 
   const lucroComComprom = (valorLiquido - quantidadeInvestidaValue).toFixed(2);
 
-  lucroSemIOF.innerHTML = `R$ ${lucroComComprom.replace(".", ",")}`;
+  // lucroSemIOF.innerHTML = `R$ ${lucroComComprom.replace(".", ",")}`;
 
-  rendimentoPorcentagemCDI(dias)
+  rendimentoPorcentagemCDI(dias);
 }
-
 
 function rendimentoPorcentagemCDI(diasInvestidos = 5) {
   const valoresTaxa = {
@@ -236,9 +268,25 @@ function rendimentoPorcentagemCDI(diasInvestidos = 5) {
     10: 185,
   };
 
-  rendimentoCDI.innerHTML = `${valoresTaxa[diasInvestidos]}% <span>do CDI</span>`
+  rendimentoCDI.innerHTML = `${valoresTaxa[diasInvestidos]}% <span>do CDI</span>`;
 }
 
-rendimentoPorcentagemCDI()
-LucroComCdb()
+rendimentoPorcentagemCDI();
+LucroComCdb();
 LucroComCompromissada();
+
+function setVariablesTable(key, value) {
+  if (key == 5) {
+    tableInfo[key].innerHTML = `<strong>R$ ${value}</strong>`;
+  } else {
+    tableInfo[key].innerHTML = `<strong>${value}</strong>`;
+  }
+}
+
+function setVariablesTableInfos(key, value, cipher = false) {
+  if(cipher){
+    tableInfo[key].innerHTML = `<strong>R$ ${value}</strong>`;
+  }else{
+    tableInfo[key].innerHTML = `<strong>${value}</strong>`;
+  }
+}
